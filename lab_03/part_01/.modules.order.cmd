2805da6802e39573c8958a50eeaac3ef64dc9b23
Polina <sirotkina.polina.2001@gmail.com>
2022-04-07 22:13:03 +0300

    Delete .modules.order.cmd

--- lab_03/part_01/.modules.order.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/polina/sem_06/bmstu_os/lab_03/part_01/modules.order := {   echo /home/polina/sem_06/bmstu_os/lab_03/part_01/md.ko; :; } | awk '!x[$$0]++' - > /home/polina/sem_06/bmstu_os/lab_03/part_01/modules.order
